6edbc8965e31dc7cd31566fd3e813b28332d45f1
robbiesh <sherrard.rob@gmail.com>
2014-10-03 17:49:36 -0400

    partial and sidebar

--- wp-content/themes/sustainable-williamson/page.php
+++ wp-content/themes/sustainable-williamson/page.php
@@ -13,23 +13,19 @@
 
       </div>
       <div class="four columns sidebar">
-        <p>Duis lobortis massa imperdiet quam. Ut varius tincidunt libero. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor.
 
-Nulla neque dolor, sagittis eget, iaculis quis, molestie non, velit. Pellentesque commodo eros a enim. Integer tincidunt.
+        <?php include_once(locate_template('partials/sidebar.php')); ?>
 
-Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-
-Ut id nisl quis enim dignissim sagittis. Phasellus gravida semper nisi. Praesent adipiscing.</p>
       </div>
 
       <?php if(get_field('content_left')) { ?>
-        <div class="three columns">
+        <div class="four columns">
           <?php the_field('content_left'); ?>
         </div>
       <?php } ?>
 
       <?php if(get_field('content_right')) { ?>
-        <div class="three columns">
+        <div class="four columns">
           <?php the_field('content_right'); ?>
         </div>
       <?php } ?>
